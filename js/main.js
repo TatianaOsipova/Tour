87db6c0dd590bcfeaea2a4a0b19d7553698007e5
Tatiana Osipova <otv120780@gmail.com>
2022-01-12 20:06:25 -0500

    feat: added slider

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $(function(){
         autoplay: true,
         fade: true
     });
+
+    $('.our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
 });
+
+
+
